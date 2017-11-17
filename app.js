f8aba5222a5b88ef1c24f3b8338efb59cfddf532
ralfvd <ralfvd@gmail.com>
2017-11-17 21:55:23 +0100

    in progress, not working yet

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const Homey = require('homey');
+//const Log = require('homey-log').Log;
+
 var fs = require('fs');
 var variableManager = require('./lib/variablemanagement/variablemanagement.js');
 var util = require('./lib/util/util.js');
@@ -12,84 +15,100 @@ var flowActions = require('./lib/flow/actions.js');
 var flowConditions = require('./lib/flow/conditions.js');
 //var flowTriggers = require('./lib/flow/triggers.js');
 
-var self = {
-  init: function() {
-	Homey.log("CountDown started");
-	variableManager.init();
+class CountDownApp extends Homey.App {
 
-	autoCompleteActions.createAutocompleteActions();
-  autoCompleteConditions.createAutocompleteConditions();
-  autoCompleteTriggers.createAutocompleteTriggers();
+  onInit() {
 
-	flowActions.createActions();
-	flowConditions.createConditions();
+    console.log(`${this.id} running...`);
 
-        Homey.manager('flow').on('trigger.countdown_to_zero', function (callback,args,state) {
-        if ( args.variable.name == state.variable ) {
-                callback(null,true);
-                return;
+    variableManager.init();
+
+    //var varCollection = Homey.ManagerSettings.get('variables');
+    //console.log(varCollection);
+  	//autoCompleteActions.createAutocompleteActions();
+    //autoCompleteConditions.createAutocompleteConditions();
+    //autoCompleteTriggers.createAutocompleteTriggers();
+
+  	//flowActions.createActions();
+  	//flowConditions.createConditions();
+
+          this.countdowntozeroFlowCardTrigger = new Homey.FlowCardTrigger('countdown_to_zero')
+            .register()
+            .registerRunListener((args, state) => {
+                if ( args.variable.name == state.variable ) {
+                  callback(null,true)
+                  return;
+                } else {
+                  callback(null, false); // true to make the flow continue, or false to abort
+              }
+              });
+
+          this.countdownstartedFlowCardTrigger = new Homey.FlowCardTrigger('countdown_started')
+           .register()
+           .registerRunListener((args, state) => {
+             if (args.variable.name == state.variable) {
+               callback(null,true);
+               return;
+             } else {
+               callback(null, false); // true to make the flow continue, or false to abort
+             }
+          });
+
+
+          this.countdownstoppedFlowCardTrigger = new Homey.FlowCardTrigger('countdown_stopped')
+           .register()
+           .registerRunListener((args, state) => {
+            if ( args.variable.name == state.variable ) {
+              callback(null,true);
+              return;
            } else {
-                callback(null, false); // true to make the flow continue, or false to abort
-         }
-        });
+          callback(null, false); // true to make the flow continue, or false to abort
+          }
+          })
 
-        Homey.manager('flow').on('trigger.countdown_started', function (callback,args,state) {
-        if (args.variable.name == state.variable) {
-          callback(null,true);
-          return;
-         } else {
-        callback(null, false); // true to make the flow continue, or false to abort
-        }
-        });
-
-        Homey.manager('flow').on('trigger.countdown_stopped', function (callback,args,state) {
-        if ( args.variable.name == state.variable ) {
-            callback(null,true);
-            return;
-         } else {
-        callback(null, false); // true to make the flow continue, or false to abort
-        }
-        });
-
-        Homey.manager('flow').on('trigger.countdown_timer_changed', function (callback,args,state) {
-        if ( args.variable.name == state.variable ) {
-            //Homey.log(args);
-            callback(null,true);
-            return;
-         } else {
-        callback(null, false); // true to make the flow continue, or false to abort
-        }
-        });
+          this.countdowntimerchangedFlowCardTrigger = new Homey.FlowCardTrigger('countdown_timer_changed')
+           .register()
+           .registerRunListener((args, state) => {
+             if ( args.variable.name == state.variable ) {
+              //Homey.log(args);
+              callback(null,true);
+              return;
+           } else {
+          callback(null, false); // true to make the flow continue, or false to abort
+          }
+          });
 
 
-	var currentVariables= variableManager.getvariables();
-  Homey.log(currentVariables.length);
-	setInterval(timers_update,1000);
-	function timers_update() {
-		var currentVariables= variableManager.getvariables();
-		//Homey.log(currentVariables);
-	        currentVariables.forEach(function( obj) {
-		 	//Homey.log(obj.name);
-			//Homey.log(obj.value);
-      var tokens = { 'variable' : obj.name, 'value' : obj.value };
-      var state = { 'variable' : obj.name };
-      if (obj.value == 0) {
-				//Homey.log("Value triggered: ",obj.value);
-				// Homey.manager('flow').trigger('countdown_test');
-				//var tokens = { 'variable' : obj.name };
-				//var state = { 'variable' : obj.name };
-				Homey.manager('flow').trigger('countdown_to_zero', tokens, state);
-        Homey.manager('flow').trigger('countdown_timer_changed', tokens, state);
-	  		variableManager.updatevariable(obj.name,'-1','number','');
-			}
-			if (obj.value > 0) {
-				variableManager.updatevariable(obj.name, obj.value - 1, 'number','');
-        Homey.manager('flow').trigger('countdown_timer_changed', tokens, state);
-			}
-		});
-	};
+    flowActions.createActions();
+    
+  	var currentVariables= variableManager.getvariables();
+    console.log(currentVariables.length);
+  	setInterval(timers_update,1000);
+  	function timers_update() {
+  		var currentVariables= variableManager.getvariables();
+  		//Homey.log(currentVariables);
+  	        currentVariables.forEach(function( obj) {
+  		 	//Homey.log(obj.name);
+  			//Homey.log(obj.value);
+        var tokens = { 'variable' : obj.name, 'value' : obj.value };
+        var state = { 'variable' : obj.name };
+        if (obj.value == 0) {
+  				//Homey.log("Value triggered: ",obj.value);
+  				// Homey.manager('flow').trigger('countdown_test');
+  				//var tokens = { 'variable' : obj.name };
+  				//var state = { 'variable' : obj.name };
+  				Homey.manager('flow').trigger('countdown_to_zero', tokens, state);
+          Homey.manager('flow').trigger('countdown_timer_changed', tokens, state);
+  	  		variableManager.updatevariable(obj.name,'-1','number','');
+  			}
+  			if (obj.value > 0) {
+  				variableManager.updatevariable(obj.name, obj.value - 1, 'number','');
+          Homey.manager('flow').trigger('countdown_timer_changed', tokens, state);
+  			}
+  		});
+  	};
+
   }
 }
 
-
-module.exports = self;
+module.exports = CountDownApp;
